9b3d87128c60da6f4cd7f774edf2534c55a92033
Samson kiran <82866972+samsonkiran02@users.noreply.github.com>
2021-08-09 13:31:13 +0530

    Update Script.js

--- Js/SK-0A02/V-1.0/Script.js
+++ Js/SK-0A02/V-1.0/Script.js
@@ -2,7 +2,7 @@
   
 	var icon = '<img class="Samson_Kiran-icon" src="https://samsonkiran02.github.io/Link/Images/PNG/Samson-Kiran.png">';
 
-	document.head.insertAdjacentHTML('beforeend','<style>.Samson_Kiran-icon { width: 40px; height: 40px; position: fixed; bottom: 15px; right: 15px; border-radius: 50px; 			  -webkit-box-shadow: 0px 0px 3px 2px white; -moz-box-shadow: 0px 0px 3px 2px white; box-shadow: 0px 0px 3px 2px white;}</style>'); 
+	document.head.insertAdjacentHTML('beforeend','<style>.Samson_Kiran-icon { width: 40px; height: 40px; position: fixed; bottom: 15px; right: 15px; border-radius: 50px; -webkit-box-shadow: 0px 0px 3px 2px white; -moz-box-shadow: 0px 0px 3px 2px white; box-shadow: 0px 0px 3px 2px white;}</style>'); 
   
 	var a = document.createElement('a');
 	a.setAttribute('href','https://samsonkiran02.github.io/0/2.html');
